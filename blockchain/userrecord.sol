b56d53a2b8d39a11cf0adb3fa3a82f789d07976e
Risav Paudel <rissuuuu@gmail.com>
2021-03-27 21:42:25 +0545

    Eth

--- blockchain/userrecord.sol
+++ blockchain/userrecord.sol
@@ -1,40 +1,44 @@
-import “stringUtils.sol”;
-
+pragma solidity ^0.8.2;
 contract UserRecords{
 
-    enum genderType {male,female};
-
+    enum genderType {male,female}
+    
     struct user{
         string name;
         genderType gender;
     }
 
-    user user_obj
-
-    function getGenderfromString (string gender) internal returns (genderType){
-        if (stringUtils.eqial(gender,"male")){
-            return genderType.male;
-        }
-        else{
-            return genderType.female;
-        }
-    }
+    user user_obj;
     
-    function getGendertoString (genderType gender) internal returns (string){
+    // function compareStrings(string  a, string  b)  returns (bool) {
+    // return (keccak256(bytes(a)) == keccak256(bytes(b)));
+
+    // }
+
+    function getGenderFromString(string memory gender) internal returns(genderType) {
+    if(keccak256(bytes(gender)) == keccak256(bytes("male"))) {
+        return genderType.male;
+    } else {
+        return genderType.female;
+    }
+}
+    
+    function getGendertoString (genderType gender) internal returns (string memory){
         if (gender==genderType.male){
             return "male";
         }
         else{
             return "female";
         }
+    }
 
-    function setUser(string name, string gender) public {
-        genderType gender_type=getGenderfromString(gender);
+    function setUser(string memory name, string memory gender) public {
+        genderType gender_type = getGenderFromString(gender);
         user_obj=user({name:name,gender:gender_type});
     
     }
     
-    function getUser() public returns (string,string){
+    function getUser() public returns (string memory,string memory){
         return (user_obj.name,getGendertoString(user_obj.gender));
     }
 
