d58b1c35cee3fc9b3bd8738edd69a0b3776e8845
Risav Paudel <rissuuuu@gmail.com>
2021-04-05 21:34:53 +0545

    P2P

--- blockchain/P2P/contract.sol
+++ blockchain/P2P/contract.sol
@@ -1,26 +1,24 @@
 pragma solidity ^0.8.2;
-contract BuyerRecords{
-    uint public dueDate;
-    uint public invoiceAmount;
-    address serviceProvider;
-    
-    
-    struct Buyer{
-        string name;
-        int buyer_property_id;
-        string seller_name;
-        int seller_energy_id;
-        int seller_property_id;
-        
+
+contract SmartInvoice {
+
+    struct User{
+        uint  dueDate;
+        uint  invoiceAmount;
+        address serviceProvider;
     }
-
-   constructor(uint _invoiceAmount) public {
-    dueDate = block.timestamp + 200;
-    invoiceAmount = _invoiceAmount;
-    serviceProvider = msg.sender;
-  }
     
+    User user_obj;
 
-    
-
+    function payAmount(uint invoiceAmount) public {
+        uint dueDate = block.timestamp;
+        uint invoiceAmount = invoiceAmount;
+        address serviceProvider = msg.sender;
+                       user_obj=User({dueDate:dueDate,invoiceAmount:invoiceAmount,
+                       serviceProvider:serviceProvider});
+                       
+    }
+    function getUser() public returns (uint,uint,address){
+            return (user_obj.dueDate,user_obj.invoiceAmount,user_obj.serviceProvider);
+        }
 }
